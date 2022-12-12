61a8eaba29cd3d7951d6cc29b6ebf253f778ea40
bahaeddine <arfaoui.bahaeddine>
2022-12-12 14:14:45 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,22 @@
-#include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdlib.h>
 /**
 *main - Entry point
-**
+*
 *Return: Always 0 (success)
 */
-int main(void) {
-	    char alph[] = "abcdefghijklmnopqrstuvwxyz";
-	        int i;
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-    for (i = 0; i < 26; i++) {
-        putchar(alph[i]);
-    if (i == 25)
-       {          			         
-	    putchar('\n');
-       }
-	return (0);
-    }
+	for (i = 0; i < 26; i++)
+		{
+	putchar(alphabet[i]);
+	if (i == 25)
+			{
+			putchar('\n');
+			}
+		}
+return (0);
 }
