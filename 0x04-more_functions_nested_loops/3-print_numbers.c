b036a14a9ca2ff113bad40641a0844d6263d3e7c
bahaeddine <arfaoui.bahaeddine>
2022-12-16 18:27:32 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	
+
+	}
+}
